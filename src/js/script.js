948543badbb4ee2a75eaf467d660438313b3fb34
learnwithaminul <learnwithaminul@gmail.com>
2025-01-04 21:44:33 +0600

    tailwind assignment

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+"use-strict"
+
+const menuButton = document.getElementById("menubtn");
+const menuitem = document.getElementById("menuitem");
+
+menuButton.addEventListener("click", ()=>{
+    menuitem.classList.toggle("hidden");
+    menuitem.classList.add()
+})
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
+      },
+      fontFamily: {
+        "sans": ["IBM Plex Sans Hebrew", "serif"]
+      },
+      colors:{
+        btnBg : "#000000D9",
+        textColor : "#000000D9",
+        primaryBg : "#B1D8FC",
+        secondaryBg : "#F5F5F5",
+        checkmarkColor : "#FFEA79",
+        darkBg : "#323232",
+        headingOrange : "#FD852F",
+        headingPurple : "#9149ED",
+        bodyText : "#7d7d7d",
+
+      }
+    },
+    container:{
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        "sm": '2rem',
+        "lg": '4rem',
+        "xl": '5rem',
+        "2xl" : '120px',
+      },
+
+    }
+  },
+  plugins: [],
+}
+
